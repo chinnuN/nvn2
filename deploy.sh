02b79a216a8b84cf9b4bd1578f5aba3cafd15f7d
Chinnu <chinnu@gmail.com>
2019-10-22 03:46:03 +0530

    deployed

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 ENV=$1
 if [ $ENV = "QA" ];then
 for i in `cat ip.txt`
@@ -6,7 +6,7 @@ for i in `cat ip.txt`
 		echo "deploy war file $i:"
 		sshpass -p "Chinnu143" scp target/nvn.war sonu@$i:/home/sonu/software/apache-tomcat-8.5.23/webapps
 		echo "server startng $i"
-		sshpass -p "Chinnu143" ssh sonu@$i "JAVA_HOME=/home/sonu/software/jdk1.8.0_151" "/home/sonu/apache-tomcat-8.5.23/bin/./startup.sh"
+		sshpass -p "Chinnu143" ssh sonu@$i "JAVA_HOME=/home/sonu/software/jdk1.8.0_151" "/home/sonu/software/apache-tomcat-8.5.23/bin/./startup.sh"
 		done
 		echo "deploy success"
 fi
