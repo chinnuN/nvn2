78bf1e60a073d323a48fcee267fcdbdefc983bfc
Chinnu <chinnu@gmail.com>
2019-10-22 03:40:02 +0530

    delpy file modified

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ for i in `cat ip.txt`
 		echo "deploy war file $i:"
 		sshpass -p "Chinnu143" scp target/nvn.war sonu@$i:/home/sonu/software/apache-tomcat-8.5.23/webapps
 		echo "server startng $i"
-		sshpass -p "Chinnu143" ssh sonu@i "JAVA_HOME=/home/sonu/software/jdk1.8.0_151" "/home/sonu/apache-tomcat-8.5.23/bin/./startup.sh"
+		sshpass -p "Chinnu143" ssh sonu@$i "JAVA_HOME=/home/sonu/software/jdk1.8.0_151" "/home/sonu/apache-tomcat-8.5.23/bin/./startup.sh"
 		done
 		echo "deploy success"
 fi
